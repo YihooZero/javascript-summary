e6872ffc046ff4ee6134d207958885d31e32e9af
Yihoo <yihoo1991@outlook.com>
2021-12-07 22:14:56 +0800

    add inheritance PrototypeChaining

--- objectOriented/03inheritance/01PrototypeChaining.js
+++ objectOriented/03inheritance/01PrototypeChaining.js
@@ -1,2 +1,21 @@
 // 原型链
+// demo1-->原型图imgs/objectOriented-03-01-01.png
+function SuperType() {
+  this.property = true;
+}
 
+SuperType.prototype.getSuperValue = function () {
+  return this.property;
+}
+
+function SubType() {
+  this.subproperty = false;
+}
+// 继承了SuperType
+SubType.prototype = new SuperType();
+SubType.prototype.getSubValue = function () {
+  return this.subproperty;
+}
+
+var instance = new SubType();
+console.log(instance.getSuperValue());
--- objectOriented/03inheritance/README.md
+++ objectOriented/03inheritance/README.md
@@ -2,4 +2,14 @@
 
 1. [原型链](https://github.com/YihooZero/javascript-summary/blob/main/objectOriented/03inheritance/01PrototypeChaining.js)
 
-​		原型链基本概念：
+​		原型链基本概念：利用原型让一个引用类型继承另一个引用类型的属性和方法。
+
+​		具体实现：让原型等于另一个类型的实例，此时原型对象将包含一个指向另一个原型的指针
+
+- **别忘记默认的原型**
+
+​		所有引用类型默认都继承了 `Object`，也是通过原型链实现的。所有函数的默认原型都是 `Object` 的实例，因此默认原型都会包含一		个内部指针，指向 `Object.prototype`。
+
+-  **确定原型和实例的关系**
+
+​		
