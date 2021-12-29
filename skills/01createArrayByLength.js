2838f0c2e8a5a606356c498500cdbedce30f617f
易虎 <hu.yi@hiscene.com>
2021-12-29 18:04:28 +0800

    add createArrayByLength one method

--- skills/01createArrayByLength.js
+++ skills/01createArrayByLength.js
@@ -4,3 +4,6 @@ Array.from({length:3})  // [undefined, undefined, undefined]
 
 // 方法二
 Array.apply(null, {length:3}) // [undefined, undefined, undefined]
+
+// 方法三
+new Array(3).fill() // [undefined, undefined, undefined]
