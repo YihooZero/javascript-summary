71e300b73528cfd5c6e5545864d5096498896f1a
易虎 <hu.yi@hiscene.com>
2023-04-06 16:27:56 +0800

    add cannot use forEach reason

--- highLevelUsage/06AsyncAwaitSequentialExecution.js
+++ highLevelUsage/06AsyncAwaitSequentialExecution.js
@@ -12,4 +12,11 @@ async function logInOrder(urls) {
   for (const textPromise of textPromises) {
     console.log(await textPromise);
   }
+
+  // NOTE: forEach 并不会按照次序输出
+  // 原因：forEach() 方法要求使用同步函数——它不会等待 promise 执行完成
+  // 详见 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  textPromises.forEach(async textPromise => {
+    console.log(await textPromise)
+  })
 }
